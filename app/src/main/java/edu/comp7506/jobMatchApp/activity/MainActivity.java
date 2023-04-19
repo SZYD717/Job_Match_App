1e5a45dd802e657f7b58b20d0735389267f88bf3
SZYD <elg55ban@126.com>
2023-04-19 20:21:06 +0800

    adding a logo for app

--- app/src/main/java/edu/comp7506/jobMatchApp/activity/MainActivity.java
+++ app/src/main/java/edu/comp7506/jobMatchApp/activity/MainActivity.java
@@ -2,7 +2,10 @@ package edu.comp7506.jobMatchApp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.job_match_app.R;
 
@@ -12,6 +15,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adminClickNum = 0;
     }
 }
