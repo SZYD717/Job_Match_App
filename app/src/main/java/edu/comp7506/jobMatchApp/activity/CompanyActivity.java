21aa210f39edb8ad21168e84ae0a107c5a6fb798
SZYD <elg55ban@126.com>
2023-04-19 10:38:44 +0800

    adding a logo for app

---
+++ app/src/main/java/edu/comp7506/jobMatchApp/activity/CompanyActivity.java
@@ -0,0 +1,16 @@
+package edu.comp7506.jobMatchApp.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.job_match_app.R;
+
+public class CompanyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_main);
+
+    }
+}
---
+++ app/src/main/java/edu/comp7506/jobMatchApp/activity/UserActivity.java
@@ -0,0 +1,17 @@
+package edu.comp7506.jobMatchApp.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.job_match_app.R;
+
+public class UserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main);
+
+
+    }
+}
