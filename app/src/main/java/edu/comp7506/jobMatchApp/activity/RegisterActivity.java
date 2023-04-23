16e1e66458de23ea437eb36dc2619e05dd6b3575
SZYD <elg55ban@126.com>
2023-04-23 12:51:01 +0800

    Register页面的前端全部写完

--- app/src/main/java/edu/comp7506/jobMatchApp/activity/RegisterActivity.java
+++ app/src/main/java/edu/comp7506/jobMatchApp/activity/RegisterActivity.java
@@ -1,16 +1,109 @@
 package edu.comp7506.jobMatchApp.activity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.job_match_app.R;
 
+import java.util.concurrent.FutureTask;
+
+import edu.comp7506.jobMatchApp.service.RegisterService;
+
 public class RegisterActivity extends AppCompatActivity {
+    private EditText usernameText;
+    private EditText phoneText;
+    private EditText mailText;
+    private EditText passwordText;
+    private EditText passwordConfirmText;
+    private TextView passwordConfirmHint;
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Intent loginIntent = new Intent(this,MainActivity.class);
+        // 在onCreate方法中获取所有EditText对象
+        usernameText = findViewById(R.id.register_usernameText);
+        phoneText = findViewById(R.id.register_phoneText);
+        mailText = findViewById(R.id.register_mailText);
+        passwordText = findViewById(R.id.register_passwordText);
+        passwordConfirmText = findViewById(R.id.register_passwordConfirmText);
+        passwordConfirmHint = findViewById(R.id.register_passwordConfirmHint);
+
+        Button registerButton = findViewById(R.id.register_registerButton);
+
+        registerButton.setOnClickListener(view -> {
+            RegisterService registerService = new RegisterService(
+                    mailText.getText().toString(),
+                    phoneText.getText().toString(),
+                    usernameText.getText().toString(),
+                    passwordText.getText().toString());
+            FutureTask<Integer> registerTask = new FutureTask<>(registerService);
+            Thread t = new Thread(registerTask);
+            t.start();
+            try{
+                int status = registerTask.get();
+                if(status==1){
+                    Toast toast = Toast.makeText(getApplicationContext(),"Register Success!",Toast.LENGTH_SHORT);
+                    toast.show();
+                    startActivity(loginIntent);
+                }
+                else{
+                    Toast toast = Toast.makeText(getApplicationContext(),"Username been used, try another one",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }catch (Exception e){
+                Log.e("RegisterTaskCall",e.getMessage());
+            }
+
+        });
+
+        passwordConfirmText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if(passwordText.getText().toString().equals(passwordConfirmText.getText().toString())){
+                    passwordConfirmHint.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    passwordConfirmHint.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(passwordText.getText().toString().equals(passwordConfirmText.getText().toString())){
+                    passwordConfirmHint.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    passwordConfirmHint.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(passwordText.getText().toString().equals(passwordConfirmText.getText().toString())){
+                    passwordConfirmHint.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    passwordConfirmHint.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/comp7506/jobMatchApp/service/RegisterService.java
@@ -0,0 +1,61 @@
+package edu.comp7506.jobMatchApp.service;
+
+import android.app.DownloadManager;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.util.concurrent.Callable;
+
+import edu.comp7506.jobMatchApp.activity.RegisterActivity;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class RegisterService implements Callable<Integer> {
+    private String email;
+    private String phone;
+    private String username;
+    private Long psw;
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    public RegisterService(String email, String phone, String username, String psw){
+        this.email = email;
+        this.phone = phone;
+        this.username = username;
+        this.psw = Long.parseLong(psw);
+    }
+    @Override
+    public Integer call() throws Exception {
+        int status = 0;
+        String url = "http://192.168.3.7:8090/register";
+
+        //request body
+        /*
+        "": "string",
+  "customerId": "000",
+  "customerName": "string",
+  "customerPassword": "000",
+  "customerRegisterTime": "2023-04-23T03:17:26.178Z",
+  "customerTele": 0,
+  "status": 0
+         */
+        JSONObject requestJson = new JSONObject();
+        requestJson.put("customerEmail",email);
+        requestJson.put("customerTele",phone);
+        requestJson.put("customerName",username);
+        requestJson.put("customerPassword",psw);
+        RequestBody requestBody = RequestBody.create(JSON,requestJson.toString());
+        try{
+            Request request = new Request.Builder().url(url).post(requestBody).build();
+            Response response = new OkHttpClient().newCall(request).execute();
+            JSONObject responseJson = new JSONObject(response.body().string());
+            status = responseJson.getInt("status");
+        }catch (Exception e){
+            Log.e("Register",e.getMessage());
+        }
+        return status;
+    }
+}
