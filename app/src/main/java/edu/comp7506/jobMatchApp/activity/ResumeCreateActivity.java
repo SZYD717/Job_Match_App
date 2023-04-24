c53c58f08b2f49f05f51b2db5ea57f1c1d63d107
SZYD <elg55ban@126.com>
2023-04-24 16:19:43 +0800

    CreateResume界面搞定

---
+++ app/src/main/java/edu/comp7506/jobMatchApp/activity/ResumeCreateActivity.java
@@ -0,0 +1,20 @@
+package edu.comp7506.jobMatchApp.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.job_match_app.R;
+
+public class ResumeCreateActivity extends AppCompatActivity {
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_resume_review);
+    }
+}
--- app/src/main/java/edu/comp7506/jobMatchApp/activity/ResumeFragment.java
+++ app/src/main/java/edu/comp7506/jobMatchApp/activity/ResumeFragment.java
@@ -1,16 +1,34 @@
 package edu.comp7506.jobMatchApp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.job_match_app.R;
 
 public class ResumeFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_resume, container, false);
+        View view = inflater.inflate(R.layout.fragment_resume, container, false);
+        Button resumeReviewButton = view.findViewById(R.id.create_resume_button);
+        resumeReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resumeCreateIntent = new Intent(view.getContext(),ResumeCreateActivity.class);
+                view.getContext().startActivity(resumeCreateIntent);
+            }
+        });
+        return view;
     }
 }
